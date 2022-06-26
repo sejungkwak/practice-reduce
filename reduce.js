9267056d1a92a355efb9b08417be6e866462ae94
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-26 00:22:41 +0000

    Summing an array of numbers, Totaling a specific object property, Grouping by a property, and totaling it too, Grouping by a property, and providing an array of property items

--- reduce.js
+++ reduce.js
@@ -7,31 +7,68 @@
 
 // Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+
+let sum = nums.reduce((acc, curr) => acc + curr);
+console.log(sum);
+
+let sumWithInitialValue = nums.reduce((acc, curr) => acc + curr, 10)
+console.log(sumWithInitialValue);
+
+// Team Members Array
 
 const teamMembers = [
-  {
-    name: 'Andrew',
-    profession: 'Developer',
-    yrsExperience: 5
-  },
-  {
-    name: 'Ariel',
-    profession: 'Developer',
-    yrsExperience: 7
-  },
-  {
-    name: 'Michael',
-    profession: 'Designer',
-    yrsExperience: 1
-  },
-  {
-    name: 'Kelly',
-    profession: 'Designer',
-    yrsExperience: 3
-  }
-];
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
 
 // Totaling a specific object property
 
+const totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0)
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience
+    }
+    return acc;
+}, {});
+
+console.log(experienceByProfession);
+
+// Grouping by a property, and providing an array of items.
+
+let employeesByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = [curr.name];
+    } else {
+        acc[key].push(curr.name);
+    }
+    return acc;
+}, {});
+
+console.log(employeesByProfession);
